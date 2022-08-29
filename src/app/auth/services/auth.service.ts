98f4d2c926c36720e0305b16de117d6094d62cb6
YonnathanDev <yonnathan.placencia.dev>
2022-08-29 16:33:16 -0400

    end commit

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthResponse, Usuario } from '../interfaces/interfaces';
+import { catchError, map, of, tap, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _usuario!: Usuario;
+
+  get usuario(){
+    return { ...this._usuario };
+  }
+
+  constructor( private http: HttpClient ) { }
+
+  registro(name: string, email: string,password: string){
+
+    const url = `${ this.baseUrl }/auth/new`;
+    const body = { name, email, password};
+    return this.http.post<AuthResponse>(url, body)
+          .pipe(
+            tap( ({ ok, token }) => {
+              if( ok ){
+                localStorage.setItem('token', token! );
+              }
+
+            }),
+            map( resp => resp.ok ),
+            catchError( err => of(err.error.msg) )
+
+          )
+
+  }
+
+
+  login( email: string, password: string ){
+
+    const url = `${ this.baseUrl }/auth`;
+    const body = { email, password };
+
+    return this.http.post<AuthResponse>( url, body)
+        .pipe(
+          tap( resp => {
+            if( resp.ok ){
+              localStorage.setItem('token', resp.token! );
+            }
+
+          }),
+          map( resp => resp.ok ),
+          catchError( err => of(err.error.msg) )
+        )
+
+  }
+
+
+  validarToken(): Observable<boolean>{
+
+    const url = `${ this.baseUrl }/auth/renew`;
+    const headers = new HttpHeaders()
+        .set('x-token', localStorage.getItem('token') || '' );
+
+    return this.http.get<AuthResponse>( url , { headers } )
+        .pipe(
+          map( resp => {
+
+            localStorage.setItem('token', resp.token! );
+            this._usuario = {
+              name: resp.name!,
+              uid: resp.uid!,
+              email: resp.email!
+            }
+            return resp.ok;
+
+          }),
+          catchError( err => of(false) )
+        );
+
+  }
+
+
+  logout(){
+    localStorage.clear();
+  }
+
+
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://mean-app-auth.herokuapp.com/api'
 };
